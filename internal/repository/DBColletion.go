d5738620635eb68ed5e1207997063fd16617d2c8
sudo-odner <odner.main@gmail.com>
2024-06-13 21:29:32 +0300

    add struct coletion

---
+++ internal/repository/DBColletion.go
@@ -0,0 +1,35 @@
+package repository
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// TODO: Create struct for "course", "lecture", "question", "answerQuestion"
+
+type Course struct {
+	ID    primitive.ObjectID `bson:"_id"`
+	Title string             `bson:"title"`
+}
+
+type Lecture struct {
+	ID          primitive.ObjectID   `bson:"_id"`
+	Title       string               `bson:"title"`
+	Description string               `bson:"description"`
+	Lector      string               `bson:"lector"`
+	QuestionsID []primitive.ObjectID `bson:"questionID"`
+	FeedbacksID []primitive.ObjectID `bson:"feedbackID"`
+}
+
+type Question struct {
+	ID    primitive.ObjectID `bson:"_id"`
+	Title string             `bson:"title"`
+}
+
+type AnswerQuestion struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	Question   string             `bson:"question"`
+	Answer     string             `bson:"answer"`
+	IsRelevant bool               `bson:"isRelevant"`
+	IsPositive bool               `bson:"isPositive"`
+	Object     string             `bson:"object"`
+}
